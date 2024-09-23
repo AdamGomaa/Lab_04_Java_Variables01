4fa988eef2de790ac72c078773936ad5138cac08
AdamGomaa <GOMAAA@MAIL.UC.EDU>
2024-09-22 20:49:52 -0400

    Added Variables

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
+        int intOperandA = 2;
+        int intOperandB =54;
+        int intSum;
+        int intProduct = 8;
+        int intDifference = 10;
+        int intQuotient = 12;
+        int intModulo = 16;
+        double doubleOperandA = 4.5;
+        double doubleOperandB = 6.5;
+        double doubleSum = 8.5;
+        double doubleProduct = 2.5;
+        double doubleDifference = 5.5;
+        double doubleQuotient = 10.5;
+
+        intSum = intOperandA + intOperandB; // Assignment that uses the arithmentic operator +
+        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum); // Output statement
+
+
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
