87d51d987f8d09250106260a79e1be24ff1f6825
AdamGomaa <GOMAAA@MAIL.UC.EDU>
2024-09-23 12:01:44 -0400

    Even Odd

---
+++ src/Lab_04_04_EvenOdd.java
@@ -0,0 +1,11 @@
+public class Lab_04_04_EvenOdd {
+    public static void main(String[] args) {
+        int numToExamine = 3;
+        if(numToExamine % 2 == 0){
+            System.out.println("The examined number is even.");
+        }
+        else{
+            System.out.println("The examined number is odd.");
+        }
+    }
+}
